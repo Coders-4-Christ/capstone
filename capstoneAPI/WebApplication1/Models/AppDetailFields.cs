8715cb17c2d7b397ad085223d55dfe5f1be3767c
JDavidson99 <75281663+Jake0427@users.noreply.github.com>
2021-04-18 14:04:55 -0400

    New Files

---
+++ capstoneAPI/WebApplication1/Models/AppDetailFields.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AppDetailFields
+    {
+        public int DetailFieldID { get; set; }
+        public int DetailID { get; set; }
+        public int ControlOrder { get; set; }
+        public int EntityFieldID { get; set; }
+        public int ControlTypeID { get; set; }
+    }
+}
